1505f87001ea9e8e43f283d392b570d2e7cbb50f
aureleplan <aurele.plan@outlook.fr>
2025-02-11 12:30:55 +0100

    Refactor GPS handling in test_gps.py; integrate Navigation class for following GPS coordinates

--- test/test_gps.py
+++ test/test_gps.py
@@ -3,6 +3,7 @@ import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+
 from calibration import gyro_calib, magnetometer_calib
 from utils import ddlib
 
@@ -13,9 +14,21 @@ print(GYRO_CALIBRATION_FILE)
 
 
 
+# gps = ddlib.GPS()
+# while True:
+#     time.sleep(1)
+#     gps.get_coords()
+#     print("coordonnées gps :", gps.gps_position,
+#           "  ||  coordonnées cartésiennes :", gps.x, gps.y, end="\r")
+    
+
+
+imu = ddlib.IMU()
 gps = ddlib.GPS()
-while True:
-    time.sleep(1)
-    gps.get_coords()
-    print("coordonnées gps :", gps.gps_position,
-          "  ||  coordonnées cartésiennes :", gps.x, gps.y, end="\r")
+arduino = arddrv.ArduinoIO()
+
+Nav = ddlib.Navigation(imu, gps, arduino)
+
+Nav.follow_gps((48.1996872, -3.0153766), False)
+
+
