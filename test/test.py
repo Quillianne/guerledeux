38c7826bbd7940be200d69bbab422d7eb097a0d1
aureleplan <aurele.plan@outlook.fr>
2025-02-11 10:48:55 +0100

    fgerhqdwnfc

--- test/test.py
+++ test/test.py
@@ -11,13 +11,3 @@ from utils import ddlib
 GYRO_CALIBRATION_FILE = os.path.join(os.path.dirname(__file__), '..',"gyro_calibration.npz")
 print(GYRO_CALIBRATION_FILE)
 
-
-
-
-
-gps = ddlib.GPS()
-while True:
-    time.sleep(1)
-    gps.get_coords()
-    print("coordonnées gps :", gps.gps_position)
-    print("coordonnées cartésiennes :", gps.x, gps.y)
