44bb873574f867035a90b6e2edc21611de469e2b
Quillianne <kili@nbarantal.me>
2025-02-11 15:58:44 +0100

    fix

--- utils/ddlib.py
+++ utils/ddlib.py
@@ -125,15 +125,15 @@ class Navigation:
         return moyenne[2]
 
     def trigger_gesture(self):
-        acc_z = self.get_z_acc_mean(self.imu)
+        acc_z = self.get_z_acc_mean()
         while acc_z > 2800:
             self.arduino_driver.send_arduino_cmd_motor(0, 0)
-            acc_z = self.get_z_acc_mean(self.imu)
+            acc_z = self.get_z_acc_mean()
             #print(acc_z)
 
         while acc_z < 3500:
             self.arduino_driver.send_arduino_cmd_motor(100, 100)
-            acc_z = self.get_z_acc_mean(self.imu)
+            acc_z = self.get_z_acc_mean()
             #print(acc_z)
 
         self.arduino_driver.send_arduino_cmd_motor(0, 0)
