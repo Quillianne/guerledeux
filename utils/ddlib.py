bd1a3b52a9a1afda8e82b4ca46a73b2c4b0d51b9
Quillianne <kili@nbarantal.me>
2025-02-11 16:53:15 +0100

    test

--- utils/ddlib.py
+++ utils/ddlib.py
@@ -185,7 +185,7 @@ class Navigation:
         self.arduino_driver.send_arduino_cmd_motor(0, 0)
         print("Navigation complete. Motors stopped.")
     
-    def follow_trajectory(self, f, fdot, duration = 300):
+    def follow_trajectory(self, f, fdot, duration = 900):
         """
         Make the boat follow a trajectory defined by a function f(t) and its derivative fdot(t).
         
