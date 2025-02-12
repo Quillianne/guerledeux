585ac08c2016e3950986bbf1d38015244bbd5721
Quillianne <kili@nbarantal.me>
2025-02-12 10:50:01 +0100

    test retour gps

--- test/test_gps.py
+++ test/test_gps.py
@@ -7,6 +7,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from calibration import gyro_calib, magnetometer_calib
 from utils import ddlib
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'drivers-ddboat-v2'))
+import arduino_driver_v2 as arddrv
 
 
 GYRO_CALIBRATION_FILE = os.path.join(os.path.dirname(__file__), '..',"gyro_calibration.npz")
@@ -14,17 +16,19 @@ print(GYRO_CALIBRATION_FILE)
 
 
 gps = ddlib.GPS()
-while True:
-    time.sleep(0.05)
-    gps.get_coords()
-    print("coordonnées gps :", gps.gps_position,
-          " | coordonnées cartésiennes :", gps.x, gps.y, end="\r")
+imu = ddlib.IMU()
+
+#while True:
+#    time.sleep(0.05)
+#    gps.get_coords()
+#    print("coordonnées gps :", gps.gps_position,
+#          " | coordonnées cartésiennes :", gps.x, gps.y, end="\r")
     
 
-#arduino = arddrv.ArduinoIO()
+arduino = arddrv.ArduinoIO()
 
-#Nav = ddlib.Navigation(imu, gps, arduino)
+Nav = ddlib.Navigation(imu, gps, arduino)
 
-#Nav.follow_gps((48.1996872, -3.0153766), False)
+Nav.follow_gps((48.1990704, -3.0149072), False)
 
 
