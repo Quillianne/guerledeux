dcfb49081c4bf720f1b4e889c6fb8e4354a31b9a
Quillianne <kili@nbarantal.me>
2025-02-12 10:18:39 +0100

    added client

--- test/circle_with_server.py
+++ test/circle_with_server.py
@@ -10,6 +10,38 @@ from circle_trajectory import circle_trajectory, circle_trajectory_dot
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'drivers-ddboat-v2'))
 import arduino_driver_v2 as arddrv
 
+import socket
+import sys
+
+
+# Robot 2 (Client)
+def robot2_client_onetime(server_ip):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, 5000))
+    print("Connected to DDBoat 17 (Server)")
+
+    # def signal_handler(sig, frame):
+    #     print("Closing client connection gracefully...")
+    #     client_socket.close()
+    #     sys.exit(0)
+    
+    # signal.signal(signal.SIGINT, signal_handler)
+
+    data = ""
+    try:
+        while True:
+            data = client_socket.recv(1024).decode()
+            if not data:
+                break
+            print("Received GPS Position from DDBoat 17: {}".format(data))
+            break
+    except Exception as e:
+        print("Client error: {}".format(e))
+    finally:
+        client_socket.close()
+    return data
+
+
 
 # Initialisation des capteurs et des moteurs
 imu = ddlib.IMU(dt = DT)
@@ -24,7 +56,7 @@ duration = 0
 t0 = time.time()
 
 def get_master_position():
-    pass
+    return robot2_client_onetime("172.20.25.217")
 
 
 print("demarrage suivi de trajectoire")
