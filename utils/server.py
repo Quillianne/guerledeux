29205cbd375052af7e15fd583277f59adad40c81
Quillianne <kili@nbarantal.me>
2025-02-12 16:29:41 +0100

    fix server

--- utils/server.py
+++ utils/server.py
@@ -4,47 +4,59 @@ import time
 import random
 import sys, os
 import signal
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'drivers-ddboat-v2'))
+
+global gps_position
 
 # DDBoat 17 (Server)
 def handle_client(conn, addr, gps):
+    global gps_position
     print("Connection established with {}".format(addr))
-
-    rmc_ok = False
-    while not rmc_ok:
-        rmc_ok, rmc_data = gps.read_rmc_non_blocking()
-        if rmc_ok:
-            gps_position = "%f;%s;%f;%s\0" % (rmc_data[0], rmc_data[1], rmc_data[2], rmc_data[3])
-            break
-        time.sleep(0.1)
     
     try:
         while True:
-            rmc_ok = False
-            while not rmc_ok:
-                rmc_ok, rmc_data = gps.read_rmc_non_blocking()
-                print ("RMC Data",rmc_data,rmc_ok)
-                if rmc_ok:
-                    gps_position = "%f;%s;%f;%s\0" % (rmc_data[0], rmc_data[1], rmc_data[2], rmc_data[3])
-                    break
-                time.sleep(0.1)            
+            # rmc_ok, rmc_data = gps.read_rmc_non_blocking()
+            # if rmc_ok:
+            #     print ("RMC Data",rmc_data,rmc_ok)
+            #     gps_position = "%f;%s;%f;%s\0" % (rmc_data[0], rmc_data[1], rmc_data[2], rmc_data[3])
             try:
                 conn.sendall(gps_position.encode())
                 print("Sent GPS Position to {}: {}".format(addr, gps_position))
+                break
             except (socket.error, BrokenPipeError):
                 print("Client {} disconnected unexpectedly.".format(addr))
                 break  # Exit loop when client disconnects
-            time.sleep(0.01)  # save computations
+            time.sleep(0.01)  # save computations (wait a bit)
     except Exception as e:
         print("Server error with {}: {}".format(addr, e))
     finally:
+        print ("Connection closed with {}".format(addr))
         conn.close()
 
+
 def robot1_server():
+    global gps_position
+    gps_position = "0.0;N;0.0;E\0"
     import gps_driver_v2 as gps_drv
     gps = gps_drv.GpsIO(tty_dev=1)
     gps.set_filter_speed("0")
+
+    def gps_reading_thread():
+        global gps_position
+        print("GPS reading thread started")
+        while True:
+            rmc_ok = False
+            try:
+                rmc_ok, rmc_data = gps.read_rmc_non_blocking()
+            except Exception as e:
+                print("GPS reading thread error: {}".format(e))
+            if rmc_ok:
+                gps_position = "%f;%s;%f;%s\0" % (rmc_data[0], rmc_data[1], rmc_data[2], rmc_data[3])
+                print ("RMC Data",rmc_data,rmc_ok)
+            time.sleep(0.1)
+    gps_thread = threading.Thread(target=gps_reading_thread)
+    gps_thread.daemon = True
+    gps_thread.start()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', 5000))  # Listen on all available interfaces
     server_socket.listen(15)  # Allow up to 15 clients
@@ -59,3 +71,4 @@ def robot1_server():
 
 if __name__ == "__main__":
     robot1_server()
+
