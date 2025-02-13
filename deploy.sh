c29d2ca5e987f14c4ce6aeec82edf8b4f63dc0be
Quillianne <kili@nbarantal.me>
2025-02-13 12:28:31 +0100

    debug deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Script pour :
-# 1. Copier grp(premierBateau) vers grp(premierBateau) sur les bateaux suivants
-# 2. Copier calib_(bateau).txt vers grp(premierBateau)/calib_(bateau).txt sur le premier bateau
-# 3. Exécuter consensus.py sur chaque bateau en se plaçant dans grp(premierBateau)
-
 ########################################
 # Lecture du fichier config.txt
 ########################################
@@ -35,8 +30,8 @@ fi
 PREMIER="${BATEAUX[0]}"
 
 ########################################
-# 1. Copie de grp(premier) en série 
-#    de BATEAUX[i] vers BATEAUX[i+1]
+# 1. Copie de grp(premier)
+#    de BATEAUX[0] vers BATEAUX[i+1]
 ########################################
 
 echo "========== ETAPE 1 : COPIES EN CHAINE DU DOSSIER grp${PREMIER} =========="
@@ -45,28 +40,41 @@ do
     SRC="${BATEAUX[$i]}"
     DST="${BATEAUX[$i+1]}"
     
-    echo "Copie de grp${PREMIER} du bateau ${SRC} vers le bateau ${DST} ..."
+    echo "Copie de grp${PREMIER} du bateau ${PREMIER} vers le bateau ${DST} ..."
     
     # Commande scp pour copier le dossier grp(PREMIER)
     # -r pour copie récursive
-    scp -r "ue32@172.20.25.2${SRC}:~/grp${PREMIER}" "ue32@172.20.25.2${DST}:~/grp${PREMIER}"
+    scp -r "ue32@172.20.25.2${PREMIER}:~/grp${PREMIER}" "ue32@172.20.25.2${DST}:~/grp${PREMIER}"
     
-    echo "=> Copie terminée de ${SRC} vers ${DST}."
+    echo "=> Copie terminée de ${PREMIER} vers ${DST}."
     echo
 done
 
-echo "========== ETAPE 2 : COPIE DES FICHIERS calib_(bateau).txt DE grp${BATEAU} VERS grp${PREMIER} (SUR CHAQUE BATEAU) =========="
-for BATEAU in "${BATEAUX[@]}"; do
-  echo "Sur le bateau ${BATEAU}, copie de ~/grp${BATEAU}/calib_${BATEAU}.txt vers ~/grp${PREMIER}/calib_${BATEAU}.txt ..."
-  
-  ssh "ue32@172.20.25.2${BATEAU}" \
-      "cp ~/grp${BATEAU}/calib_${BATEAU}.txt ~/grp${PREMIER}/calib_${BATEAU}.txt"
+########################################
+# 2. Copie des fichiers calib_(bateau).txt
+#    vers grp(PREMIER) (sur chaque bateau)
+########################################
 
-  if [ $? -eq 0 ]; then
-    echo "=> Fichier calib_${BATEAU}.txt copié avec succès dans ~/grp${PREMIER}/ sur le bateau ${BATEAU}."
-  else
-    echo "=> ERREUR : Impossible de copier calib_${BATEAU}.txt sur le bateau ${BATEAU}."
-  fi
+echo "========== ETAPE 2 : RECHERCHE ET COPIE DES FICHIERS calib_(bateau).txt VERS ~/grp${PREMIER} =========="
+for BATEAU in "${BATEAUX[@]}"; do
+  echo "Sur le bateau ${BATEAU}, recherche de calib_${BATEAU}.txt dans tous les dossiers grp*..."
+
+  ssh "ue32@172.20.25.2${BATEAU}" bash -c "'
+    found=\$(find ~ -type f -name \"calib_${BATEAU}.txt\" -path \"*grp*\" | head -n 1)
+    if [ -n \"\$found\" ]; then
+      echo \"Fichier trouvé : \$found\"
+      echo \"Copie vers ~/grp${PREMIER}/calib_${BATEAU}.txt...\"
+      cp \"\$found\" ~/grp${PREMIER}/calib_${BATEAU}.txt
+      if [ \$? -eq 0 ]; then
+        echo \"=> Copie réussie : \$found -> ~/grp${PREMIER}/calib_${BATEAU}.txt\"
+      else
+        echo \"=> ERREUR : Échec lors de la copie de \$found\"
+      fi
+    else
+      echo \"=> ERREUR : Aucun calib_${BATEAU}.txt trouvé dans ~ (ou pas dans un dossier grp*)\"
+    fi
+  '"
+
   echo
 done
 
