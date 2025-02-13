1c2f5c16b4b90b5cd578da53c877412e41c1a6ae
Quillianne <kili@nbarantal.me>
2025-02-13 09:48:58 +0100

    start consensus

---
+++ consensus.py
@@ -0,0 +1,23 @@
+import os, sys
+import time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+from calibration import gyro_calib, magnetometer_calib
+from utils import ddlib
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'drivers-ddboat-v2'))
+import arduino_driver_v2 as arddrv
+
+
+
+
+gps = ddlib.GPS()
+imu = ddlib.IMU()
+
+arduino = arddrv.ArduinoIO()
+
+Nav = ddlib.Navigation(imu, gps, arduino)
+
+Nav.follow_gps((48.19904833333333, -3.0148149999999996), cartesian = False, distance = 6)
